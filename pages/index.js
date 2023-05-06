2158be291e43e37e67c69a69b5bd02acbca2fcff
David Goldstein <goldstein.g.david@gmail.com>
2023-05-06 16:46:44 -0400

    Add sample blogposts with static metadata on the homepage

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,19 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    }
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -19,6 +29,21 @@ export default function Home() {
           Anyhow, I've built this nifty personal site by following the <a href="https://nextjs.org/learn">Next.js tutorial</a>!
           Feel free to take a look around, and perhaps check out this sample <Link href="/posts/first-post">Blog Post</Link>!
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
