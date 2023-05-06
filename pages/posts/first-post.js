3fc24ea58a782c8a3467098ef7b1a5061885cc81
David Goldstein <goldstein.g.david@gmail.com>
2023-05-06 16:09:42 -0400

    Color within the lines

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,26 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
+
+import Layout from '../../components/layout';
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <ProfilePic />
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
+    </Layout>
   );
 }
+
+const ProfilePic = () => (
+  <Image
+    src="/images/profile.jpg"
+    height={144}
+    width={144}
+    alt="David"
+  />
+);
