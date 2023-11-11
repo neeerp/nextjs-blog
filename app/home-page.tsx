ea7841a72b6be0db8ea05f4f6931f5b8b9e2022b
David Goldstein <goldstein.g.david@gmail.com>
2023-11-11 11:30:25 -0500

    Use Tailwind for home page; breaks blog post pages

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 
 import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
 
 export default function Home({
   allPostsData,
@@ -21,28 +20,29 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
+      <section className="text-xl/tight">
+        <p className="my-5">
           Hey there, I&apos;m David! I&apos;m a full time Software Engineer 💻
           and hobbyist Squirrel Tamer 🐿️!When I&apos;m not writing code or busy
           raising an army of squirrels, I&apos;m probably losing track of time
           exploring a rabbithole of vim configurations.
         </p>
-        <p>
+        <p className="my-5">
           Anyhow, I&apos;ve built this nifty personal site by following the{" "}
-          <a href="https://nextjs.org/learn"> Next.js tutorial </a>! Feel free
-          to take a look around, and perhaps check out one of my posts below!
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>! Feel free to
+          take a look around, and perhaps check out one of my posts below!
         </p>
       </section>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}> Blog </h2>
-        <ul className={utilStyles.list}>
+      <section className="text-xl/tight pt-px">
+        <h2 className="text-2xl/snug font-bold my-4"> Blog </h2>
+        <ul>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
+            <li className="mb-5" key={id}>
+              {/**<li className={utilStyles.listItem} key={id}>**/}
               <Link href={`/posts/${id}`}> {title} </Link>
               <br />
-              <small className={utilStyles.lightText}>
+              <small className="text-gray-500">
                 <Date dateString={date} />
               </small>
             </li>
